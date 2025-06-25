79d894da9cc593aaa13cb7729178369740921f68
Oceane Drapeau <ocedrapeau@gmail.com>
2025-06-25 15:32:44 +0200

    update model with add

--- model.py
+++ model.py
@@ -9,16 +9,36 @@ def connect_db():
     )
 
     mycursor = mydb.cursor()
+    return mydb, mycursor
 
-    mycursor.execute("SELECT * FROM Countries")
-    countries = mycursor.fetchall()
-    print(countries)
+def disconnect_db(mydb, mycursor):
+    mycursor.close()
+    mydb.close()
 
-    print('-------')
+def actionBDD(action, key="", infos = {}) :
+    mydb, mycursor = connect_db()
+    if not infos and not key == "" :
+        data = action(mycursor, key)
+        print("Doing action : " + str(action) , ". Using data = action(mycursor, key) with key = ", key)
 
-    for country in mycursor:
-        print(country)
+    elif not infos and key=="":
+        action(mydb, mycursor)
+        print("Doing action : " + str(action) + ". Using action(mydb, mycursor) no returns")
 
+    elif infos and key != "" :
+        data = action(mydb, mycursor, key, infos)
+        print("Doing action : " + str(action) + ". Using data = action(mydb, mycursor, key, infos) with key = ", key, ", infos = ", infos)
+
+    disconnect_db(mydb, mycursor)
+    return data
+
+def get(mycursor, key):
+    if key == "c" :
+        get_countries(mycursor)
+    elif key == "l" :
+        get_languages(mycursor)
+    elif key == "poi" :
+        get_points_of_interest(mycursor)
     
 def get_countries(mycursor):
     mycursor.execute("SELECT * FROM Countries")
@@ -44,25 +64,40 @@ def get_point_of_interest_by_name(name, mycursor):
     mycursor.execute("SELECT * FROM InterestPoints WHERE nameInterestPoint = '" + name + "'")
     result = mycursor.fetchone()
     return result
+
+def add(mydb, mycursor, key, infos):
+    if key == "c" :
+        add_country(mydb, mycursor, infos)
+    elif key == "l" :
+        add_language(mydb, mycursor, infos)
+    elif key == "poi" :
+        add_point_of_interest(mydb, mycursor, infos)
  
 def add_country(mydb, mycursor, infos): 
     mycursor.execute('''SELECT MAX(idCountry) FROM Countries''')
-    id = mycursor.fetchone()[0] + 1
-    mycursor.execute('''INSERT INTO Countries VALUES (''' + id +''',"''' + infos.name + '''","''' + infos.desc + '''",''' + infos.inhab + ''',''' + "'" + infos.date + "'" +'''"'''+ infos.gouv + '''"''' +''')''')
+    infos.id = mycursor.fetchone()[0] + 1
+    mycursor.execute('''INSERT INTO Countries VALUES (''' + infos.id +''',"''' + infos.name + '''","''' + infos.desc + '''",''' + infos.inhab + ''',''' + "'" + infos.date + "'" +'''"'''+ infos.gouv + '''"''' +''')''')
     for i in infos.lang :
-        mycursor.execute('''INSERT INTO Countries_has_languages VALUES (''' + id +''',''' + infos.lang[i] + ''',"''' + infos.lang[i+1] +''')''') 
+        mycursor.execute('''INSERT INTO Countries_has_languages VALUES (''' + infos.id +''',''' + infos.lang[i] + ''',"''' + infos.lang[i+1] +''')''') 
     mydb.commit()
 def add_language(mydb, mycursor, infos): 
     mycursor.execute('''SELECT MAX(idLanguage) FROM Languages''')
-    id = mycursor.fetchone()[0] + 1
-    mycursor.execute('''INSERT INTO Languages VALUES (''' + id +''',"''' + infos.name + '''","''' + infos.gender + '''",''' + infos.order + ''')''')
+    infos.id = mycursor.fetchone()[0] + 1
+    mycursor.execute('''INSERT INTO Languages VALUES (''' + infos.id +''',"''' + infos.name + '''","''' + infos.gender + '''",''' + infos.order + ''')''')
     mydb.commit()
 def add_point_of_interest(mydb, mycursor, infos): 
     mycursor.execute('''SELECT MAX(idInterestPoint) FROM InterestPoints''')
-    id = mycursor.fetchone()[0] + 1
-    mycursor.execute('''INSERT INTO Languages VALUES (''' + id +''',"''' + infos.name + '''",''' + "'"+ infos.date +"'"+ ''',"''' + infos.desc + '''",''' + infos.type + ''',''' + infos.coun +''')''')
+    infos.id = mycursor.fetchone()[0] + 1
+    mycursor.execute('''INSERT INTO Languages VALUES (''' + infos.id +''',"''' + infos.name + '''",''' + "'"+ infos.date +"'"+ ''',"''' + infos.desc + '''",''' + infos.type + ''',''' + infos.coun +''')''')
     mydb.commit()
 
+def delete(mydb, mycursor, key, infos):
+    if key == "c" :
+        delete_country(mydb, mycursor, infos.id)
+    elif key == "l" :
+        delete_language(mydb, mycursor, infos.id)
+    elif key == "poi" :
+        delete_point_of_interest(mydb, mycursor, infos.id)
 
 def delete_country(mydb, mycursor, id):
     mycursor.execute('''DELETE FROM Countries_has_languages WHERE idCountry= '''+ id)
