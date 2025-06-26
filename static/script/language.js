6b1c97db30955247c1ecfe223b5e41e21637d9c1
Akiliyh <akiliyhsensei@gmail.com>
2025-06-26 13:57:36 +0200

    added js integration

--- static/script/language.js
+++ static/script/language.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    const poiId = poiData.id;
+
     document.getElementById("delete-btn").addEventListener("click", async () => {
-        const poiId = poiData.id;
         fetch('/api/language/' + poiId, {
             method: 'DELETE',
         }).then(res => res.text())
@@ -9,4 +11,26 @@ document.addEventListener("DOMContentLoaded", () => {
                 window.location.href = "/";
             })
     });
+
+    fetch('/api/language/' + poiId, {
+        method: 'GET',
+    })
+    .then(res => res.json())
+    .then(data => {      
+        let desc = document.querySelector('.desc');
+        let inh = document.querySelector('.inhabitants');
+        let gov = document.querySelector('.gov');
+        let date = document.querySelector('.date');
+        let capital = document.querySelector('.capital');
+        let name = document.querySelector('.name');
+        
+        desc.textContent = data.descCountry;
+        inh.textContent = data.inhabitants + ' inhabitants';
+        gov.textContent = data.governmentType;
+        date.textContent = data.date;
+        capital.textContent = data.capital;
+        name.textContent = data.nameCountry;
+        document.title = data.nameCountry;
+
+    })
 });
